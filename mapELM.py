aae9607ded2b014961a8b54a04e184b583fbc47c
Nicolas Palopoli <nicopalo@gmail.com>
2015-10-08 19:16:47 -0300

    [MINOR] Moved code into mapELMpositions function

--- mapELM.py
+++ mapELM.py
@@ -13,17 +13,15 @@
 import sys
 import csv
 from collections import OrderedDict
-from parseJPred import readFasta, readJPred
 
 # Read input files
 try:
   inELMinstances = open(sys.argv[1])
-#  injnet = open(sys.argv[2])
 except IndexError:
-  print("Input file not specified. Format: ./mapELM.py <elm_instances[.date].tsv>")
+  print("Input file not specified. Format: ./mapELM.py <elm_instances[.date].tsv> <primaryAcc>")
   exit()
 except IOError:
-  print("Input file not found. Format: ./mapELM.py <elm_instances[.date].tsv>")
+  print("Input file not found. Format: ./mapELM.py <elm_instances[.date].tsv> <primaryAcc>")
   exit()
 
 try:
@@ -33,36 +31,21 @@ except IndexError:
   exit()
 
 def readELMinstances(infile):
-#  with open(infile, 'rb') as csvfile:
+  '''Store ELM instances information as list of dicts'''
   elm = csv.DictReader(filter(lambda row: row[0]!='#', infile),delimiter='\t', quotechar='"')
-#  for row in elm:
-#    print row
   return elm
-#    for row in reader:
-#    if row['Primary_Acc'] == 'Q63767':
-#      print row['Primary_Acc'],row['Start'],row['End']
-
-# Make dict of input files
-parsedELM = readELMinstances(inELMinstances)
-inELMinstances.close()
-#predictions = readJPred(injnet)
-#injnet.close()
-#results = seq.copy()
-#results.update(predictions)
 
 def mapELMpositions(parsedELM,primaryAcc):
+  '''Make dict with [ELMAccession:(Start,End)]'''
   ELMpos = {}
   for row in parsedELM:
     if primaryAcc == row['Primary_Acc']:
       ELMpos[row['Accession']] = (row['Start'],row['End'])
-#      ELMpos['Start'] = row['Start']
-#      ELMpos['End'] = row['End']
   return ELMpos
 
+# Make list of dicts from input files
+parsedELM = readELMinstances(inELMinstances)
+inELMinstances.close()
+# Print dict of start and end positions of ELMs for Primary Accession
 ELMpos = mapELMpositions(parsedELM,primaryAcc)
 print ELMpos
-
-# Print table with results
-#for row in zip(*([key] + value for key, value in results.items())):
-#  print '\t'.join(map(str, row))
-
